1e1897941814f23fc3545d9f80fd9ec4b379d261
grouppp8 <83763302+grouppp8@users.noreply.github.com>
2023-04-22 15:42:05 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -98,7 +98,7 @@ def check_new_image(image, model):
 # .title() is used to get the input text string
 if(st.button('Predict')):
     if uploaded_file_1 is not None and uploaded_file_2 is not None:
-        check_img = spectral.envi.open(uploaded_file_1,uploaded_file_2).load()
+        check_img = spectral.envi.open(uploaded_file_1.name,uploaded_file_2.name).load()
         #st.write(check_img.shape)
         st.success(check_new_image(check_img, model))
     else:
